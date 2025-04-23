058f405aa5058dd570a9614c2d3adb406ed6408a
Phúc Phạm <phuc31082002@gmail.com>
2025-04-23 22:53:55 +0700

    chore(cdn): add fontawsome cdn and website icon

--- resources/views/Quan_ly_tai_khoan/them_tai_khoan.blade.php
+++ resources/views/Quan_ly_tai_khoan/them_tai_khoan.blade.php
@@ -7,6 +7,8 @@
   <title>Thêm tài khoản</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="icon" href="{{ asset('imgs/paw-solid.svg') }}" type="image/x-icon">
   <link rel="stylesheet" href="{{ asset('css/quan_ly_tai_khoan.css') }}">
 
 </head>
