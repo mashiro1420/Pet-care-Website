53babdf3c0e2188126b911d48a2276cad220b380
DoDucManh <mashiro1420@gmail.com>
2025-04-26 16:59:47 +0700

    feat(dich_vu): update dich_vu controller

--- app/Http/Controllers/DichVuController.php
+++ app/Http/Controllers/DichVuController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CSDichVuModel;
+use App\Models\TCDichVuModel;
 use Illuminate\Http\Request;
 
 class DichVuController extends Controller
 {
-    public function viewQuanLy()
+    public function viewQuanLy( Request $request)
+    {
+        $queryChamSoc = CSDichVuModel::select('*','dm_dichvu.id as dich_vu_id')
+        ->leftJoin('dm_dichvu','ql_dichvuchamsoc.id_dich_vu','=','dm_dichvu.id');
+        $queryTrongCoi = TCDichVuModel::select('*','dm_dichvu.id as dich_vu_id')
+        ->leftJoin('dm_dichvu','ql_dichvutrongcoi.id_dich_vu','=','dm_dichvu.id');
+        $data['cham_socs'] = $queryChamSoc->get();
+        $data['trong_cois'] = $queryTrongCoi->get();
+        return view('Quan_ly_dich_vu.quan_ly_dich_vu', $data);
+    }
+    public function xlThemChamSoc( Request $request )
+    {
+
+    }
+    public function xlThemTrongCoi( Request $request )
+    {
+        
+    }
+    public function xlXoaChamSoc( Request $request )
+    {
+        
+    }
+    public function xlXoaTrongCoi( Request $request )
     {
         
     }
